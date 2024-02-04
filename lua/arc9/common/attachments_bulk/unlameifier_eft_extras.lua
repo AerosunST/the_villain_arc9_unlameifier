36536d0804f9da6fd971c367cb08906954aca764
Aerosun <83192028+AerosunST@users.noreply.github.com>
2024-02-04 06:28:36 -0600

    New toys, located in EFT Custom Slot
    adds Firemodes, RPMs, and Tracers

--- lua/arc9/common/attachments_bulk/unlameifier_eft_extras.lua
+++ lua/arc9/common/attachments_bulk/unlameifier_eft_extras.lua
@@ -423,3 +423,662 @@ ATT.Attachments = {
 
 ATT.Category = {"eft_custom_slot"}
 ARC9.LoadAttachment(ATT, "unlameifier_eft_doom_hold")
+
+///////////////////////////////////////      Extra Firemodes
+
+ATT = {}
+
+ATT.PrintName = "Full Auto"
+ATT.CompactName = "Full Auto"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Full auto. That's it. There's really nothing else to this. Is this a cheat? idk, probably. Were you going to ask for it anyway? Yes.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = -1, PoseParam = 2},
+    { Mode = 1, PoseParam = 2 }
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_full_auto")
+
+ATT = {}
+
+ATT.PrintName = "Semi-Auto"
+ATT.CompactName = "Semi-Auto"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Semi-auto. That's it. There's really nothing else to this. Is this a cheat? Highly unlikely. Were you going to ask for it anyway? No.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = 1, PoseParam = 2 }
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_semi_auto")
+
+ATT = {}
+
+ATT.PrintName = "3 Round Burst"
+ATT.CompactName = "3-Burst"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[3 Round Burst. That's it. There's really nothing else to this. Is this a cheat? idk, you like burst or something? Were you going to ask for it anyway? Probably.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = 3, PoseParam = 2 },
+	{ Mode = 1, PoseParam = 2 },
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_3rb")
+
+ATT = {}
+
+ATT.PrintName = "Navy Trigger Group"
+ATT.CompactName = "Navy"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Full Auto, 3 Round Burst, and Semi-Auto all bundled together. For the man who can't decide whether he wants to burst his weapon or mag dump it.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = -1, PoseParam = 2 },
+	{ Mode = 3, PoseParam = 2 },
+	{ Mode = 1, PoseParam = 2 },
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_navy")
+
+ATT = {}
+
+ATT.PrintName = "2 Round Burst"
+ATT.CompactName = "2-Burst"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[2 Round Burst. Kinda like the KRISS Vector but minus the funny full-auto.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = 2, PoseParam = 2 },
+	{ Mode = 1, PoseParam = 2 },
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_2rb")
+
+ATT = {}
+
+ATT.PrintName = "KRISS Vector Trigger Group"
+ATT.CompactName = "Vector"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Full Auto, 2 Round Burst, and Semi-Auto all bundled together. Kinda like the Navy Trigger Group but you work at Valve Software.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = -1, PoseParam = 2 },
+	{ Mode = 2, PoseParam = 2 },
+	{ Mode = 1, PoseParam = 2 },
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_vector")
+
+ATT = {}
+
+ATT.PrintName = "Multi-Burst"
+ATT.CompactName = "M-Burst"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Bursts divisible by 30. Have fun.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Firemodes"
+
+-- ATT.firemodes = { { Mode = -1 }, { Mode = 1 } } -- auto, semi
+ATT.Firemodes = {
+    { Mode = 30, PoseParam = 2 },
+	{ Mode = 15, PoseParam = 2 },
+	{ Mode = 10, PoseParam = 2 },
+	{ Mode = 6, PoseParam = 2 },
+	{ Mode = 3, PoseParam = 2 },
+	{ Mode = 1, PoseParam = 2 },
+}
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_firemode_mburst")
+
+///////////////////////////////////////      RPMs
+
+ATT = {}
+
+ATT.PrintName = "300 RPM"
+ATT.CompactName = "300"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[300 RPM. Slow but not too slow.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 300
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_300")
+
+ATT = {}
+
+ATT.PrintName = "600 RPM"
+ATT.CompactName = "600"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[600 RPM. Standard RPM of AK-family rifles chambered in 7.62x39mm, in case you needed an example.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 600
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_600")
+
+ATT = {}
+
+ATT.PrintName = "650 RPM"
+ATT.CompactName = "650"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[600 RPM. Standard RPM of AK-family rifles chambered in 5.45x39mm, in case you needed an example.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 650
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_650")
+
+ATT = {}
+
+ATT.PrintName = "770 RPM"
+ATT.CompactName = "770"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[770 RPM. Not sure if any gun uses this RPM... but it feels good so it's going to be an option.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 770
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_770")
+
+ATT = {}
+
+ATT.PrintName = "800 RPM"
+ATT.CompactName = "800"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[800 RPM. Standard RPM of M4-family rifles chambered in 5.56x45mm, in case you needed an example.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 800
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_800")
+
+ATT = {}
+
+ATT.PrintName = "850 RPM"
+ATT.CompactName = "850"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[850 RPM. RPM of the HK416A5 rifle, in case you needed an example. Weird how this fires faster than an M4...]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 850
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_850")
+
+ATT = {}
+
+ATT.PrintName = "1200 RPM"
+ATT.CompactName = "1200"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[1200 RPM. RPM of the Glock 18C and KRISS Vector, in case you needed an example. Do you REALLY need to be shooting this fast...?]]
+ATT.SortOrder = 1
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 1200
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_1200")
+
+ATT = {}
+
+ATT.PrintName = "6000 RPM"
+ATT.CompactName = "6000"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[6000 RPM. Because you were going to ask anyways. Pretty performance intensive, not gonna lie. Server owners be wary of this.]]
+ATT.SortOrder = 1
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/RPM"
+
+ATT.RPM = 6000
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_rpm_6000")
+
+///////////////////////////////////////      Tracers
+
+ATT = {}
+
+ATT.PrintName = "Red Tracers"
+ATT.CompactName = "Red"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun red tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(222, 31, 31)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_red")
+
+ATT = {}
+
+ATT.PrintName = "Green Tracers"
+ATT.CompactName = "Green"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun green tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(31, 222, 73)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_green")
+
+ATT = {}
+
+ATT.PrintName = "Blue Tracers"
+ATT.CompactName = "Blue"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun blue tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(31, 57, 222)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_blue")
+
+ATT = {}
+
+ATT.PrintName = "Yellow Tracers"
+ATT.CompactName = "Yellow"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun yellow tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(222, 217, 31)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_yellow")
+
+ATT = {}
+
+ATT.PrintName = "Orange Tracers"
+ATT.CompactName = "Orange"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun orange tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(222, 136, 31)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_orange")
+
+ATT = {}
+
+ATT.PrintName = "Purple Tracers"
+ATT.CompactName = "Purple"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun purple tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(124, 31, 222)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_purple")
+
+ATT = {}
+
+ATT.PrintName = "Pink Tracers"
+ATT.CompactName = "Pink"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun pink tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(222, 31, 213)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_pink")
+
+ATT = {}
+
+ATT.PrintName = "White Tracers"
+ATT.CompactName = "White"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun white tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(255, 255, 255)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_white")
+
+ATT = {}
+
+ATT.PrintName = "Cyan Tracers"
+ATT.CompactName = "Cyan"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun cyan tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(31, 180, 222)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_cyan")
+
+ATT = {}
+
+ATT.PrintName = "Teal Tracers"
+ATT.CompactName = "Teal"
+ATT.Icon = Material("entities/arc9/unlameifier/arc9_unlameifier_unfinished.png", "mips smooth")
+ATT.Description = [[Gives your gun teal tracers.]]
+ATT.SortOrder = 0
+ATT.MenuCategory = "ARC9 - EFT Attachments"
+ATT.Category = {"eft_custom_slot", }
+ATT.Folder = "Unlameifier/Tracers"
+
+ATT.TracerNum = 1
+ATT.TracerSize = 8
+ATT.TracerColor = Color(31, 222, 188)
+
+ATT.Attachments = {
+    {
+        PrintName = "Custom slot",
+        Pos = Vector(0, 0, 1),
+        Ang = Angle(0, 0, 0),
+        Category = {"eft_custom_slot"},
+    },
+}
+
+ARC9.LoadAttachment(ATT, "unlameifier_tracer_teal")
